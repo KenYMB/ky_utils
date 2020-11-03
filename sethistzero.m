b6a510757696e1781f54d49c6ae50bdcf53985f8
KenYMB <yuasa.research@gmail.com>
2020-11-03 10:39:24 -0500

    minor update

--- sethistzero.m
+++ sethistzero.m
@@ -1,12 +1,17 @@
-function    sethistzero(h,BinWidth)
+function    varargout = sethistzero(h,BinWidth)
 % SETHISTZERO(h)
 % SETHISTZERO(h,BinWidth)
 %   adjust axis limits of histogram so that no bin is placed over zero.
 %   Not appicable for bivariate histograms.
+% 
+% xl = SETHISTZERO(h)
+% [xl1,xl2] = SETHISTZERO(h)
+%   returns limits of the adjusted axis.
 
 % 201103: Yuasa
 
 narginchk(1,inf);
+nargoutchk(0,2);
 assert(isgraphics(h,'histogram'), 'h must be a hundle of histogram');
 
 if nargin > 1
@@ -21,3 +26,8 @@ ul   = ceil(max(llul)/roundunit)*roundunit;
 hA.XLim = [ll ul];
 h.BinWidth = roundunit;
 
+if nargout == 1
+    varargout = {[ll ul]};
+elseif nargout == 2
+    varargout = {ll ul};
+end
